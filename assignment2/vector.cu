d189052c46c639681f9eec0810533dfae2021ea8
Joshua Saxton <jsaxton1@students.kennesaw.edu>
2017-02-15 22:51:51 -0500

    Added file for assignment 2

---
+++ assignment2/vector.cu
@@ -0,0 +1,21 @@
+/ System includes
+#include <stdio.h>
+#include <assert.h>
+
+// CUDA runtime
+#include <cuda_runtime.h>
+
+// Helper functions and utilities to work with CUDA
+#include <helper_functions.h>
+
+
+void vecMultiply(float 
+
+int main()
+{
+	
+	int size = n * sizeof(float);
+	
+
+}
+
